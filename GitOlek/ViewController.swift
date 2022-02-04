6d00d6d2378bce2122cace5a2719ba32abf689e5
user-has-no-name <zavazhenski@gmail.com>
2022-02-04 12:08:57 +0100

    Initial Commit

---
+++ GitOlek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitOlek
+//
+//  Created by Oleksandr Zavazhenko on 04/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
